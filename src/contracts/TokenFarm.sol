3f7d084bfc9b60be445e914a4e3fae65dde9d906
mastrmind-dev <asapthaka@gmail.com>
2021-12-03 12:40:43 +0530

    stake tokens, update stake balance, update stakers list

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,15 +1,38 @@
 pragma solidity ^0.5.0;
 
-import './DaiToken.sol';
-import './DappToken.sol';
+import "./DaiToken.sol";
+import "./DappToken.sol";
 
-contract TokenFarm{
+contract TokenFarm {
     string public name = "Dapp Token Farm";
     DappToken public dappToken;
     DaiToken public daiToken;
 
-    constructor (DappToken _dappToken, DaiToken _daiToken) public {
+    address[] public stakers;
+    mapping(address => uint256) public stakingBalance;
+    mapping(address => bool) public hasStaken;
+    mapping(address => bool) public isStaking;
+
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
     }
+
+    //stake tokens
+    function stakeTokens(uint256 _amount) public {
+        daiToken.transformFrom(msg.sender, address(this), _amount);
+
+        stakingBalance[msg.sender] = _amount + stakingBalance[msg.sender];
+
+        if (!hasStaken[msg.sender]) {
+            staker.push(msg.sender);
+        }
+
+        isStaking[msg.sender] = true;
+        hasStaken[msg.sender] = true;
+    }
+
+    //2. unstake tokens
+
+    //3.give interests
 }
