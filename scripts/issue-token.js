ea68b997a85a812d5e94375788ef57b8c80575df
mastrmind-dev <asapthaka@gmail.com>
2021-12-05 09:55:11 +0530

    wrote a script to owner of the smart contract for handling issuing tokens

--- scripts/issue-token.js
+++ scripts/issue-token.js
@@ -1,9 +1,8 @@
-const TokenFarm = artifacts.require('TokenFarm')
+const TokenFarm = artifacts.require("TokenFarm");
 
 module.exports = async function(callback) {
-  let tokenFarm = await TokenFarm.deployed()
-  await tokenFarm.issueTokens()
-  // Code goes here...
-  console.log("Tokens issued!")
-  callback()
-}
+  let tokenFarm = await TokenFarm.deployed();
+  await tokenFarm.issueTokens();
+  console.log("Tokens Issued!");
+  callback();
+};
